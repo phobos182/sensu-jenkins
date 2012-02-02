56e89d3d43e24b87776ea43894b0273cd1c4f210
Jeremy Carroll <jcarroll@Jeremy-Carrolls-MacBook-Pro.local>
2012-02-02 13:48:57 -0600

    Fixing path issues

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,7 @@
 
 # SETUP
 cd $WORKSPACE
+export PATH=$PATH:$WORKSPACE:$WORKSPACE/scripts
 
 # DOWNLOAD GEM DEPENDENCIES
 bundle package --verbose
@@ -10,7 +11,10 @@ bundle package --verbose
 gem build sensu.gemspec
 
 ## PACKAGE SENSU ##
-fpm --iteration $BUILD_NUMBER --gem-bin-path=/usr/bin -a x86_64 -t rpm -s gem ./sensu*.gem
+SENSUGEM=`ls sensu*.gem`
+
+echo "** PACKAGING ** $SENSUGEM ..."
+fpm --iteration $BUILD_NUMBER --gem-bin-path=/usr/bin -a x86_64 -t rpm -s gem $SENSUGEM > /dev/null 2>&1
 
 ## PACKAGE SENSU DEPENDENCIES ##
 build-deps.sh x86_64
